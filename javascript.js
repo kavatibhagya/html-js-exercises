ed6c59bd9785fa2163b9b2537989a2b6cf5fd96d
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-01-16 22:12:14 -0800

    added button for bgcolor chnage

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const colorBtn = document.querySelector('.colorBtn');
+const bodyBcg = document.querySelector('body');
+   const colors =['yellow','red','blue','green','violet','purple','peach','#3b3746'];
+   colorBtn.addEventListener('click',changeColor);
+function changeColor ()
+    {
+        let random=Math.floor(Math.random()*colors.length);
+      bodyBcg.style.backgroundColor=colors[random];
+    }
