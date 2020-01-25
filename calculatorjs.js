95ae8edc2ba662b840d8a70af4846df1552cd676
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-01-24 23:00:06 -0800

    added calculator java script

---
+++ calculatorjs.js
@@ -0,0 +1,32 @@
+const btns=document.querySelectorAll('.btn');
+const screen=document.querySelector('.screen');
+const equalbtn=document.querySelector('.btn-equal');
+const clearbtn=document.querySelector('.btn-clear');
+
+for( let i=0; i<btns.length; i++)
+{
+    btns[i].addEventListener('click',function()
+    {
+        let number=btns[i].getAttribute('data-num');
+        screen.value+=number;
+    
+    })
+}
+equalbtn.addEventListener('click',function()
+{
+    if(screen.value==="")
+    {
+      alert("no value in the screen");
+    }
+    else{
+
+         let value=eval(screen.value);
+          screen.value=value;
+        }
+
+})
+clearbtn.addEventListener('click',function()
+{
+    screen.value="";
+
+})
